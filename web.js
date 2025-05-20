f0de3c4f15642e0ce5e7d52ad8c6d8bb916713e7
gima <gima@naver.com>
2025-05-20 20:12:15 +0900

    fix: change dist root

--- web.js
+++ web.js
@@ -5,18 +5,9 @@ const port = process.env.PORT || 8001;
 
 app.use(express.static(path.join(__dirname, '/dist')));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist/index.html"));
-});
 
-app.get("/armories", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist/armories.html"));
-});
 
-app.get("/event", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist/event.html"));
-});
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
+  });
 
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
