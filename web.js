5315313e0e9e776e5a57ad3bb6ace0264b5bcc0b
gima <gima@naver.com>
2025-05-20 20:03:17 +0900

    fix: change default port to 8001

--- web.js
+++ web.js
@@ -1,17 +1,22 @@
-let express = require("express");
-
+const path = require("path");
+const express = require("express");
 const app = express();
+const port = process.env.PORT || 8001;
 
-app.use(express.static(path.join(__dirname, '/build')));
+app.use(express.static(path.join(__dirname, '/dist')));
 
-app.get("/",(req, res)=>{
-    res.sendFile(path.join(__dirname, "build/index.html"));
-})
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist/index.html"));
+});
 
-app.get("/armories",(req, res)=>{
-    res.sendFile(path.join(__dirname, "build/armories.html"));
-})
+app.get("/armories", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist/armories.html"));
+});
 
-app.get("/event",(req, res)=>{
-    res.sendFile(path.join(__dirname, "build/event.html"));
-})
+app.get("/event", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist/event.html"));
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
